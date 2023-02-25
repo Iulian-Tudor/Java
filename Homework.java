78419d73c6d264d826a3b2c9c43d30d22394b03c
Iulian-Tudor <scutarutudor@yahoo.com>
2023-02-25 13:22:12 +0200

    Finished Homework (i think)

--- Homework.java
+++ Homework.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Homework {
 
-    static void Square(int n)
+    /*static void Square(int n)
     {
         int k = n+1;
 
@@ -22,13 +22,40 @@ class Homework {
             k--;
             System.out.println();
         }
-    }
+    } */
 
     public static void main (String[] args)
     {   
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Square(n);
+        //Square(n);
+        System.out.println();
+        
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = (i + j) % n + 1;
+            }
+        }
+
+        long startTime = System.nanoTime();
+        for (int i = 0; i < n; i++) {
+            StringBuilder rowString = new StringBuilder();
+            StringBuilder colString = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                rowString.append(matrix[i][j]).append(" ");
+                colString.append(matrix[j][i]).append(" ");
+            }
+            System.out.println("Row " + (i+1) + ": " + rowString.toString());
+            System.out.println("Col " + (i+1) + ": " + colString.toString());
+        }
+        long endTime = System.nanoTime();
+        long elapsedTime = endTime - startTime;
+        if (elapsedTime < 1000000) {
+            System.out.println("Elapsed time: " + elapsedTime + " ns");
+        } else {
+            System.out.println("Elapsed time: " + elapsedTime / 1000000 + " ms");
+        }
     }
     
 }
